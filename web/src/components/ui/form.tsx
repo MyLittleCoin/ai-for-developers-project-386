c8544e6c9442058eaa78d1c10a00bcf985d00c46
Timofey Korzhebin <korzhebin@t8.ru>
2026-08-16 00:19:18 +0000

    feat(web): admin event-types table, create dialog, detail page + tests

--- web/src/components/ui/form.tsx
+++ web/src/components/ui/form.tsx
@@ -41,9 +41,9 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState } = useFormContext()
+  const { getFieldState, formState } = useFormContext()
 
-  const fieldState = getFieldState(fieldContext.name)
+  const fieldState = getFieldState(fieldContext.name, formState)
 
   if (!fieldContext) {
     throw new Error("useFormField should be used within <FormField>")
